a4766c30c4b5f68debf9c3b29e117b1a8bdfdaa4
sanil singh <sanilsingh3008@gmail.com>
2025-04-06 20:31:03 +0530

    site analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/react"
 import Sidebar from "@/components/sidebar"
 import "./globals.css"
 
